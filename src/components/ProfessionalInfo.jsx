20bd47c8762d3c4780b17ed63dd54ff6cd28a1ec
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-10-09 19:18:07 -0300

    add professional info component

---
+++ src/components/ProfessionalInfo.jsx
@@ -0,0 +1,73 @@
+import '../styles/ProfessionalInfo.css'
+
+function Category({title, items}) {
+  return (
+    <div className={title.split(" ").join("-").toLowerCase()}>
+      <h2>{title}</h2>
+      <ul>
+      {items.map((item) => {
+        return (
+        <li key={item.id}>
+          <div>
+          <h3>{item.title}</h3>
+          <p className='place-year'>{item.place} | {item.dates.from} - {item.dates.to}</p>
+          <p className='description'>{item.description}</p>
+          </div>
+          </li>
+          )
+      })}
+      </ul>
+    </div>
+  )
+}
+
+export default function ProfessionalInfo() {
+  const example = [
+    {
+      type: 'Education',
+      items: [
+        {
+          id: crypto.randomUUID(),
+          place: 'School Name',
+          title: 'Title',
+          dates: {from: '199x', to: '200x'},
+          description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium, cupiditate.',
+        },
+        {
+          id: crypto.randomUUID(),
+          place: 'University Name',
+          title: 'Software Engineer',
+          dates: {from: '200x', to: '200x'},
+          description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem neque voluptates possimus perspiciatis, rerum odit.',
+        },
+      ],
+    },
+    {
+      type: 'Work Experience',
+      items: [
+        {
+          id: crypto.randomUUID(),
+          place: 'Company Name',
+          title: 'Job Position',
+          dates: {from: '199x', to: '200x'},
+          description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium, cupiditate.',
+        },
+        {
+          id: crypto.randomUUID(),
+          place: 'Company Name',
+          title: 'Frontend Web Developer',
+          dates: {from: '200x', to: '201x'},
+          description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem neque voluptates possimus perspiciatis, rerum odit.',
+        },
+      ],
+    },
+  ]
+
+  return (
+    <div className="professional-info">
+      {example.map((category) => {
+        return <Category key={category.type} title={category.type} items={category.items}/>
+      })}
+    </div>
+  )
+}
