a825d57b288b444892661a99965066e5bf7ddb16
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-10-09 19:18:40 -0300

    import professional info component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Applicant from "./components/Applicant";
 import PersonalInfo from "./components/PersonalInfo";
+import ProfessionalInfo from "./components/ProfessionalInfo";
 
 function App() {
   return (
     <main>
       <Applicant/>
       <PersonalInfo/>
+      <ProfessionalInfo/>
     </main>
   )
 }
